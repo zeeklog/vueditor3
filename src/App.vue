ab830991bdc69f28f9ea9df7419d56ff7ad2badd
NEO <001@usd.dog>
2022-07-15 13:59:46 +0800

    v1.0.5

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './views/HelloWorld.vue'
 </script>
 
 <template>
     <div class="wrapper" style="padding: 50px;text-align: center">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      <h1 style="text-align: center">An Easy-use UEditor Component, with vue3.x support.</h1>
       <HelloWorld msg="You did it!" />
     </div>
 </template>
